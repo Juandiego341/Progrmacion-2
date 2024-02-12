de6153ef0d655697652f8ec3805e6a81cdcd299b
Juandiego341 <158749621+Juandiego341@users.noreply.github.com>
2024-02-12 11:25:41 -0500

    Update variables.js

--- variables.js
+++ variables.js
@@ -1,7 +1,1 @@
-const prompt = require('prompt-sync')();
-var edad = parseInt(prompt("Ingrese su edad"));
-if (edad>=18) {
-    console.log("usted es  mayor de edad");
-}else{
-    console.log("usted es menor")
-}
+
